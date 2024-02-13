747a6ad8ce5dc52525944c0506f601b289510673
almazongit <124905485+almazongit@users.noreply.github.com>
2024-02-13 22:53:24 +0500

    Add files via upload

---
+++ factorial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Formulas
+{
+    public static double factorial(int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * factorial(n - 1);
+        }
+    }
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Введите X = ");
+        double x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите N = ");
+        double n = Convert.ToDouble(Console.ReadLine());
+
+        double sum = 0;
+        for (int i = 0; i <= n; i++)
+        {
+            sum += (Math.Pow(x, i)) / factorial(i);
+        }
+        Console.WriteLine("Ответ = " + sum);
+    }
+}
